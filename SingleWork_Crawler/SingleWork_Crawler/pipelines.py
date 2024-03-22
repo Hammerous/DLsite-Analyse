436db585dece7893ceee1c2c63113db1d5a6c03e
Hammerous <79643219+Hammerous@users.noreply.github.com>
2024-03-22 16:20:24 +0800

    Update pipelines.py
    chinese to english

--- SingleWork_Crawler/SingleWork_Crawler/pipelines.py
+++ SingleWork_Crawler/SingleWork_Crawler/pipelines.py
@@ -20,25 +20,24 @@ class SingleworkCrawlerPipeline:
 
     def open_spider(self, spider):
         self.json_file = codecs.open(spider.crawl_file_path +'.json', 'w+', encoding='UTF-8')
-        # 在爬虫开始时，首先写入一个 '[' 符号，构造一个 json 数组
-        # 为使得 Json 文件具有更高的易读性，我们辅助输出了 '\n'（换行符）
         self.json_file.write('[\n')
+        # input bracket and line feed at the head of .json file
 
-    #处理每一个item用的函数
+    #process every item
     def process_item(self, item, spider):
         item_json = json.dumps(dict(item), ensure_ascii=False)
         self.json_file.write('\t' + item_json + ',\n')
         return item
     
     def close_spider(self, spider):
-        # 在结束后，需要对 process_item 最后一次执行输出的 “逗号” 去除
-        # 当前文件指针处于文件尾，我们需要首先使用 SEEK 方法，定位文件尾前的两个字符（一个','(逗号), 一个'\n'(换行符)）的位置
+        # remove the comma in the end
+        # seek the position of the last two characters (a comma and a line feed) when the file pointer is at EOF
         self.json_file.seek(-2, os.SEEK_END)
-        # 使用 truncate() 方法，将后面的数据清空
+        # clear all data by truncate()
         self.json_file.truncate()
-        # 重新输出'\n'，并输入']'，与 open_spider(self, spider) 时输出的 '['，构成一个完整的数组格式
+        # input bracket and line feed at the end of .json file
         self.json_file.write('\n]')
-        # 关闭文件
+        # file closed
         self.json_file.close()
         
         time_now = time.time()
