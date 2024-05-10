0b48018e75e2b40e5c34084597ac17dc12b2ee73
Hammerous <79643219+Hammerous@users.noreply.github.com>
2024-05-11 00:24:33 +0800

    Important update
    previous versions omit some of crucial data

---
+++ Works_Crawler/SingleWork_Crawler_V1.7/SingleWork_Crawler/pipelines.py
@@ -0,0 +1,77 @@
+#pipeline of v1.0
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import time,json,codecs,os
+
+class SingleworkCrawlerPipeline:
+    Time_TMP = int(0)
+
+    def __init__(self):
+        SingleworkCrawlerPipeline.Time_TMP = time.time()    
+        local_time = time.localtime(SingleworkCrawlerPipeline.Time_TMP)
+        timeStr = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+        #timeStr_name = time.strftime("%Y%m%d_%H%M%S", local_time)
+        print("Program Start Time: "+ timeStr)
+
+    def open_spider(self, spider):
+        self.json_file = codecs.open(spider.crawl_folder_path +'.json', 'w+', encoding='UTF-8')
+        # 在爬虫开始时，首先写入一个 '[' 符号，构造一个 json 数组
+        # 为使得 Json 文件具有更高的易读性，我们辅助输出了 '\n'（换行符）
+        self.json_file.write('[\n')
+
+    #处理每一个item用的函数
+    def process_item(self, item, spider):
+        item_json = json.dumps(dict(item), ensure_ascii=False)
+        self.json_file.write('\t' + item_json + ',\n')
+        spider.pbar_download.update(1)
+        return item
+    
+    def close_spider(self, spider):
+        spider.pbar_request.refresh()
+        spider.pbar_download.refresh()
+        spider.pbar_request.close()
+        spider.pbar_download.close()
+        # 在结束后，需要对 process_item 最后一次执行输出的 “逗号” 去除
+        # 当前文件指针处于文件尾，我们需要首先使用 SEEK 方法，定位文件尾前的两个字符（一个','(逗号), 一个'\n'(换行符)）的位置
+        self.json_file.seek(-2, os.SEEK_END)
+        # 使用 truncate() 方法，将后面的数据清空
+        self.json_file.truncate()
+        # 重新输出'\n'，并输入']'，与 open_spider(self, spider) 时输出的 '['，构成一个完整的数组格式
+        self.json_file.write('\n]')
+
+        print('Checking data integrity...')
+        # 读取JSON文件中的所有ID
+        self.json_file.seek(0)  # 移动到文件开头
+        try:
+            data = json.load(self.json_file)
+            json_ids = {int(item['ID']) for item in data}
+        except json.JSONDecodeError:
+            json_ids = set()
+        # 关闭文件
+        self.json_file.close()
+        # 检查work_dict中的ID是否在json_ids中
+        missing_ids = set(spider.work_dict.keys()) - json_ids
+        # 如果有缺失的ID，写入一个新的txt文件
+        if missing_ids:
+            print("Missing: {0}".format(missing_ids))
+            with open(spider.crawl_folder_path + '_missing_ids.txt', 'w') as f:
+                for missing_id in missing_ids:
+                    f.write(str(missing_id) + '\n')
+        else:
+            print("All IDs Checked!")
+        
+        time_now = time.time()
+        local_time =  time.localtime(time_now)
+        timeStr = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+        print("Pipeline End Time: "+ timeStr)
+        Running_interval = int(time_now - SingleworkCrawlerPipeline.Time_TMP)
+        m, s = divmod(Running_interval, 60)
+        h, m = divmod(m, 60)
+        print('Total Running Time: %d:%02d:%02d'% (h, m, s))
---
+++ Works_Crawler/SingleWork_Crawler_V1.7/SingleWork_Crawler/spiders/WorksCrawler.py
@@ -0,0 +1,225 @@
+import scrapy,json,demjson3,datetime,os
+from tqdm.autonotebook import tqdm
+import pandas as pd
+from ..items import SingleWork
+
+def get_julian_day(date_string):
+    date_obj = datetime.datetime.strptime(date_string, "%Y-%m-%d")
+    julian_day = date_obj.toordinal() + 1721424.5
+    return julian_day+1
+
+class WorkcrawlerSpider(scrapy.Spider):
+    name = "WorksCrawler"
+    allowed_domains = ["dojindb.net"]
+    start_urls = ["http://dojindb.net/w/"]
+    work_dict = {}
+    crawl_folder_path = None
+    pbar_request = None
+    pbar_download = None
+
+    #Initialize class
+    def __init__(self, csv_path = None, *args, **kwargs):
+        self.work_dict = {}
+        self.crawl_folder_path = None
+        self.pbar_request = None
+        self.pbar_download = None
+        
+        super(WorkcrawlerSpider, self).__init__(*args, **kwargs)
+        if csv_path is None:
+            raise ValueError("A CSV Path must be provided")
+        # CSV read in
+        self.crawl_folder_path = os.path.splitext(csv_path)[0]
+        try:
+            df = pd.read_csv(csv_path)
+            # check required data
+            if "ID" in df.columns:
+                for index, row in df.iterrows():
+                    self.work_dict[row["ID"]] = {col: str(row[col]) for col in df.columns if col != "ID"}
+            else:
+                missing_columns = [col for col in ["ID", "rank", "sales"] if col not in df.columns]
+                print(f"Missing columns {missing_columns} in {csv_path}")
+        except Exception as e:
+            print(f"Error reading {csv_path}: {e}")
+    
+    def historyRecord(self, log_record):
+        single_time_stamp = {"site":"",
+                            "code":"",
+                            "price":"",
+                            "amount":"",
+                            "time_ymd":"",
+                            "campaign":"",
+                            "amount_diff":"",
+                            "value":""
+                            }
+        if("price" in log_record and "time_ymd" in log_record and "amount" in log_record):
+            single_time_stamp["price"] = log_record["price"]
+            single_time_stamp["amount"] = log_record["amount"]
+            single_time_stamp["time_ymd"] = str(log_record["time_ymd"])
+            julianDate = int(get_julian_day(log_record["time_ymd"]))
+        else:
+            return None,-1
+        if("site" in log_record):
+            single_time_stamp["site"] = str(log_record["site"])
+        if("code" in log_record):
+            single_time_stamp["code"] = str(log_record["code"])
+        if("campaign" in log_record):
+            single_time_stamp["campaign"] = str(log_record["campaign"])
+        if("amount_diff" in log_record):
+            single_time_stamp["amount_diff"] = str(log_record["amount_diff"])
+        if("value" in log_record):
+            single_time_stamp["value"] = str(log_record["value"])
+        return single_time_stamp,julianDate
+
+    #Initialize request
+    def start_requests(self):
+        self.pbar_request = tqdm(total=len(self.work_dict), desc=" Request Progress", leave=True,\
+                                 bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}]',position = 0 ,mininterval = 0.5)
+        self.pbar_download = tqdm(total=len(self.work_dict), desc="Download Progress", leave=True,\
+                                 bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}]',position = 1, mininterval = 0.5)
+        for key,value in self.work_dict.items():
+            first_urls = "https://dojindb.net/w/{}".format(key)
+            start_urls = first_urls
+            #print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time())) + " start request: " + start_urls)
+            #self.pbar_request.set_description(f'Work: {workID}')
+            self.pbar_request.update(1)
+            yield scrapy.Request(start_urls, callback=lambda response,workID=key,extra_info=value: self.parse(response,workID,extra_info), meta={"dont_redirect": True, "handle_httpstatus_list": [301],"dont_filter": True})
+
+    def dataserie_check(self,time_label_price,dlsite_price,fanza_price):
+        if(len(time_label_price)-len(dlsite_price) == 1):
+            dlsite_price.append('None')
+        if(len(time_label_price)-len(fanza_price) == 1):
+            fanza_price.append('None')
+        if(len(time_label_price)!=len(fanza_price)!=len(dlsite_price)):
+            raise ValueError('price seire ERROR!')
+
+    def convert_url(url):
+        parts = url.split('=')
+        domains = parts[0].split("/")
+        indexs = parts[1].split("/")
+        if 'www.dmm.co.jp' == domains[2]:
+            cid_index = indexs.index('cid') + 1
+            return indexs[cid_index]
+        elif 'www.dlsite.com' == domains[2]:
+            id_index = parts.index('id') + 1
+            return id_index
+        else:
+            return "Unknown URL format"
+
+    def parse(self, response, workID, extra_info):
+        item = SingleWork()
+        item["ID"] =  str(workID)
+        #obtain main genre and title
+        title_span = response.xpath('//span[@class="work_title"]')
+        item["main_genre"] = title_span.xpath('.//span[@class="label label-lg label-genre"]/text()').get()
+        item["title"] = title_span.xpath('.//text()').getall()[-1].strip()
+        release_info = response.xpath('//table[@class="table table-rsp mb15"]/tr')
+
+        item["release_dtl"] = {}
+        for sitediff in release_info:
+            work_price = sitediff.xpath('.//td/span[@class="work-price"]/text()').get().replace("円","").replace(",","")
+            sale_date = sitediff.xpath(".//td/span[@style='font-size:14px;']/text()")
+            sales = sale_date[0].get().split(":")[1].replace(",","").replace(" ","")
+            released_date = sale_date[1].get().replace("年","-").replace("月","-").replace("日","").replace("\n","").replace(" ","")
+            site_url = sitediff.xpath(".//@data-href").get()
+            parts = site_url.split("/")
+            if 'www.dmm.co.jp' == parts[2]:
+                RJ_seiral = parts[-2].split("=")[1]
+            elif 'www.dlsite.com' == parts[2]:
+                RJ_seiral = parts[-1].split(".")[0]
+            else:
+                RJ_seiral = "Unknown URL format"
+            item["release_dtl"].update({RJ_seiral:{'work price':work_price,'date':released_date,'url':site_url,'sales':sales}})
+ 
+        #obtain circle
+        select_xpath = response.xpath('//div[@class="col-sm-5 mb20 work_detail"]')
+        #link = select_xpath.xpath(".//a[@itemprop="item"]/@href").get()
+        item["circle"] = select_xpath.xpath('.//table[@class="table mb0"]/tr/td[2]/a/text()').get().strip()
+        #obtain price data
+        """
+        #below extracts data of sellings in current figure(these data are incorperated in historic data)
+        #extract JSON string from JavaScript
+        json_string = response.xpath("//script[contains(., "var barChartData =")]/text()").re_first(r"var barChartData = (\{.*?\});")
+        data = json.loads(json_string)
+        time_labels_sale = data["labels"]
+        data_sale = data["datasets"]
+        """
+
+        #below extracts data of prices
+        json_string = response.xpath('//script[contains(., "var barChartData_pc =")]/text()').re_first(r'var barChartData_pc = (\{[\s\S]*?\});')
+        data = demjson3.decode(json_string) #convert javascript string to json object
+        time_label_price = data["labels"]
+        dlsite_price = data["datasets"][0]["data"]
+        dlsite_price = ['None' if x is demjson3.undefined else str(x) for x in dlsite_price]
+        fanza_price = data["datasets"][1]["data"]
+        fanza_price = ['None' if x is demjson3.undefined else str(x) for x in fanza_price]
+        self.dataserie_check(time_label_price,dlsite_price,fanza_price)
+        item["price_data"] = {"time": time_label_price, 
+                              "dlsite": dlsite_price, 
+                              "fanza": fanza_price
+                              }
+        #obtain main tags
+        main_tags = []
+        #xpath below is the relative element path, scrapy can automatically match it to the specific content
+        tags_box = response.xpath('//div[@class="tags_box mb15"]')
+        # scan all "a" tags
+        for tag in tags_box.xpath('.//a[@class="label label-tags"]'):
+            # text conten of the tags
+            main_tags.append(tag.xpath(".//text()").get())
+            #link = tag.xpath(".//@href").get()
+        item["main_tags"] = main_tags
+
+         #obtain historic options
+        historyData = []
+        #xpath below is the relative element path, scrapy can automatically match it to the specific content
+        select_xpath = "//div[@class='col-sm-3 text-right']/select[@class='form-control graph-range']"
+        options = response.xpath(select_xpath + "/option")
+        for option in options:
+            option_str = option.xpath("@value").get()
+            link_urls = [(option_str, idx, "https://dojindb.net/w/{0}?mode=getgraph&g={1}&site={2}".format(workID, option_str,idx)) for idx in range(1,len(item["release_dtl"])+1)]
+            historyData.extend(link_urls)
+            #text = option.xpath("text()").get()
+        #print(f"Value: {value}, Text: {text}")
+        item["historyData"] = {}
+        item["extra_info"] = extra_info
+        # start iterating requests
+        return self.parse_links(response, item, historyData)
+
+    def parse_links(self, response, item, historyData):
+        # pop out the next item if there is any
+        if historyData:
+            # period and url in next item
+            (period, site_idx, link_url) = historyData.pop()
+            request = scrapy.Request(link_url, callback=self.parse_history, dont_filter=True)
+            request.meta["item"] = item
+            request.meta["site_idx"] = site_idx
+            request.meta["period"] = period
+            request.meta["links_to_follow"] = historyData
+            return request
+        else:
+            # all items processed
+            return item
+
+    def parse_history(self, response):
+        # extract information from responsed data
+        link_data = json.loads(response.text)
+        item = response.meta["item"]
+        if(link_data["log"]):
+            single_history_data = {"log":{},"price_sum":-1,"amount_sum":-1}
+            for log_record in link_data["log"]:
+                single_time_stamp, julianDate = self.historyRecord(log_record)
+                if single_time_stamp is None:
+                     # all items processed
+                    return item
+                single_history_data["log"].update({julianDate:single_time_stamp})
+            single_history_data["price_sum"] = link_data["price_sum"]
+            single_history_data["amount_sum"] = link_data["amount_sum"]
+            # yielding data into item
+            site_idx = response.meta["site_idx"]
+            period = response.meta["period"]
+            if period not in item["historyData"]:
+                item["historyData"].update({period:{}})
+            item["historyData"][period][site_idx] = single_history_data
+        
+        # process items left
+        links_to_follow = response.meta["links_to_follow"]
+        return self.parse_links(response, item, links_to_follow)
